3881f552ed2cb7d91858efb8908c70defaaf70f9
simonnewman <sim.newman@me.com>
2021-02-08 19:38:56 +0000

    Initial set up of Springboot book exercises

--- src/main/java/SpamCheckerApplication.java
+++ src/main/java/SpamCheckerApplication.java
@@ -1,13 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SpamCheckerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<String> spamWords = new ArrayList<String>();
-        spamWords.add("illegal");
+        if (args.length == 2) {
+            spamWords = Files.readAllLines(Paths.get(args[1]));
+        }else{
+            spamWords.add("illegal");
+        }
 
-       SimpleSpamDetector simpleSpamDetector = new SimpleSpamDetector(spamWords);
+        SimpleSpamDetector simpleSpamDetector = new SimpleSpamDetector(spamWords);
         System.out.println(simpleSpamDetector.containsSpam(args[0]));
     }
 }
